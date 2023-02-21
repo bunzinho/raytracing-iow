de8359120b03446d4438cdcd98f96051d5564f51
bunzinho <76852519+bunzinho@users.noreply.github.com>
2023-02-21 12:17:24 -0500

    add ray, color, vec3 headers

---
+++ src/ray.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "vec3.h"
+
+class ray
+{
+public:
+	ray() {}
+	ray(const vec3& origin, const vec3& direction)
+		: orig(origin), dir(direction)
+	{}
+
+	vec3 origin() const
+	{
+		return orig;
+	}
+
+	vec3 direction() const
+	{ 
+		return dir;
+	}
+
+	vec3 at(float t) const
+	{
+		return orig + t * dir;
+	}
+
+public:
+	vec3 orig;
+	vec3 dir;
+};
