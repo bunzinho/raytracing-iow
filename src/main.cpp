e3f864509224a55c8bcc101a3242d6374342752c
bunzinho <76852519+bunzinho@users.noreply.github.com>
2023-03-06 15:53:03 -0500

    color sphere with surface normal

--- src/main.cpp
+++ src/main.cpp
@@ -5,28 +5,34 @@
 #include "ray.h"
 #include "vec3.h"
 
-bool hit_sphere(const vec3& center, const float radius, const ray& r)
+float hit_sphere(const vec3& center, const float radius, const ray& r)
 {
 	auto oc = r.origin() - center;
-	auto a = dot(r.direction(), r.direction());
-	auto b = 2 * dot(oc, r.direction());
-	auto c = dot(oc, oc) - radius * radius;
-	auto discriminant = b*b - 4*a*c;
-	return (discriminant > 0);
+	auto a = r.direction().length_squared();
+	auto b = dot(oc, r.direction());
+	auto c = oc.length_squared() - radius*radius;
+	auto discriminant = b*b - a*c;
+	if (discriminant < 0)
+	{
+		return -1.0f;
+	}
+	return (-b - sqrt(discriminant)) / a;
 }
 
 color ray_color(const ray& r)
 {
-	if (hit_sphere(vec3(0, 0, -1), 0.5, r))
+	auto t = hit_sphere(vec3(0, 0, -1), 0.5f, r);
+	if (t > 0.0f)
 	{
-		return color(1, 0, 0);
+		vec3 N = unit_vector(r.at(t) - vec3(0, 0, -1));
+		return 0.5f * color(N.x() + 1, N.y() + 1, N.z() + 1);
 	}
 	auto unit_direction = unit_vector(r.direction());
-	auto t = 0.5f * (unit_direction.y() + 1.0f);
+	t = 0.5f * (unit_direction.y() + 1.0f);
 	return (1.0f - t) * color(1.0f, 1.0f, 1.0f) + t * color(0.5f, 0.7f, 1.0f);
 }
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
 	// Image
 
