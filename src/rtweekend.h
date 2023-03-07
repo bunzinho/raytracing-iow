b09df53190ac2112aae696b358e534b6cf3eb98f
bunzinho <76852519+bunzinho@users.noreply.github.com>
2023-03-07 06:47:30 -0500

    add rng functions

--- src/rtweekend.h
+++ src/rtweekend.h
@@ -3,17 +3,28 @@
 #include <cmath>
 #include <limits>
 #include <memory>
+#include <cstdlib>
 
 using std::shared_ptr;
 using std::make_shared;
 using std::sqrt;
 
 constexpr float infinity = std::numeric_limits<float>::infinity();
-constexpr float pi = 3.14159265358979323846;
+constexpr float pi = 3.14159265358979323846f;
 
 constexpr inline float degrees_to_radians(float degrees)
 {
 	return degrees * pi / 180.0f;
+}
+
+inline float random_float()
+{
+	return rand() / (RAND_MAX + 1.0f);
+}
+
+inline float random_float(float min, float max)
+{
+	return min + (max - min) * random_float();
 }
 
 #include "ray.h"
