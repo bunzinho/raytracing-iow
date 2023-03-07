1d08df479a446cad9372aedfe623a0ce4dca9f39
bunzinho <76852519+bunzinho@users.noreply.github.com>
2023-03-07 08:50:50 -0500

    sample multiple rays per pixel

--- src/color.h
+++ src/color.h
@@ -3,9 +3,19 @@
 #include <iostream>
 #include "vec3.h"
 
-void write_color(std::ostream& out, color pixel_color)
+void write_color(std::ostream& out, color pixel_color, int samples_per_pixel)
 {
-	out << static_cast<int>(std::round(255.0f * pixel_color.x())) << ' '
-		<< static_cast<int>(std::round(255.0f * pixel_color.y())) << ' '
-		<< static_cast<int>(std::round(255.0f * pixel_color.z())) << ' ';
+	auto r = pixel_color.x();
+	auto g = pixel_color.y();
+	auto b = pixel_color.z();
+
+	// Divide the color by the number of samples.
+	auto scale = 1.0f / samples_per_pixel;
+	r *= scale;
+	g *= scale;
+	b *= scale;
+
+	out << static_cast<int>(std::round(255.0f * clamp(r, 0.0f, 1.0f))) << ' '
+		<< static_cast<int>(std::round(255.0f * clamp(g, 0.0f, 1.0f))) << ' '
+		<< static_cast<int>(std::round(255.0f * clamp(b, 0.0f, 1.0f))) << ' ';
 }
--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 #include "color.h"
 #include "hittable_list.h"
 #include "sphere.h"
+#include "camera.h"
 
 color ray_color(const ray& r, const hittable& world)
 {
@@ -25,8 +26,9 @@ int main(int argc, char* argv[])
 
 	const auto filename = "output.ppm";
 	const auto aspect_ratio = 16.0f / 9.0f;
-	const auto image_height = 400;
+	const auto image_height = 360;
 	const auto image_width = static_cast<int>(image_height * aspect_ratio);
+	const int samples_per_pixel = 100;
 
 	// World
 
@@ -36,14 +38,7 @@ int main(int argc, char* argv[])
 
 	// Camera
 
-	const auto viewport_height = 2.0f;
-	const auto viewport_width = aspect_ratio * viewport_height;
-	const auto focal_length = 1.0f;
-
-	auto origin = vec3(0, 0, 0);
-	auto horizontal = vec3(viewport_width, 0, 0);
-	auto vertical = vec3(0, viewport_height, 0);
-	auto lower_left_corner = origin - horizontal/2 - vertical/2 - vec3(0, 0, focal_length);
+	camera cam;
 
 	// Render 
 
@@ -56,12 +51,15 @@ int main(int argc, char* argv[])
 
 		for (int i = 0; i < image_width; ++i)
 		{
-			auto u = float(i) / (image_width - 1);
-			auto v = float(j) / (image_height - 1);
-			ray r = ray(origin, lower_left_corner + u * horizontal + v * vertical - origin);
-
-			auto pixel_color = ray_color(r, world);
-			write_color(ppmfile, pixel_color);
+			auto pixel_color = color(0, 0, 0);
+			for (int s = 0; s < samples_per_pixel; s++)
+			{
+				auto u = (i + random_float()) / (image_width - 1);
+				auto v = (j + random_float()) / (image_height - 1);
+				auto r = cam.get_ray(u, v);
+				pixel_color += ray_color(r, world);
+			}
+			write_color(ppmfile, pixel_color, samples_per_pixel);
 		}
 		ppmfile << std::endl;
 	}
