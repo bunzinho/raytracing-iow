940cc7d6509d76b53758bf6239283b39e6ecaca5
bunzinho <76852519+bunzinho@users.noreply.github.com>
2023-06-15 18:45:17 -0400

    shorten color.h code

--- src/color.h
+++ src/color.h
@@ -5,16 +5,12 @@
 
 void write_color(std::ostream& out, color pixel_color, int samples_per_pixel)
 {
-	auto r = pixel_color.x();
-	auto g = pixel_color.y();
-	auto b = pixel_color.z();
-
 	// Divide the color by the number of samples.
-	auto scale = 1.0 / samples_per_pixel;
-	r = sqrt(r * scale);
-	g = sqrt(g * scale);
-	b = sqrt(b * scale);
-
+	const auto scale = 1.0 / samples_per_pixel;
+	const auto r = sqrt(pixel_color.x() * scale);
+	const auto g = sqrt(pixel_color.y() * scale);
+	const auto b = sqrt(pixel_color.z() * scale);
+	
 	out << static_cast<uint8_t>(std::round(255 * clamp(r, 0.0, 1.0)))
 		<< static_cast<uint8_t>(std::round(255 * clamp(g, 0.0, 1.0)))
 		<< static_cast<uint8_t>(std::round(255 * clamp(b, 0.0, 1.0)));
